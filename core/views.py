a50b1b6a1241fc209ff70137d9cf62435324175b
johanna0711 <jjnarvaez@est.itsgg.edu.ec>
2020-08-15 17:24:28 -0500

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request, plantilla="home.html"):
+    return render(request, plantilla)
+
+
+def index(request, plantilla="index.html"):
+    return render(request, plantilla)
+
+
+def about(request, plantilla="about.html"):
+    return render(request, plantilla)
+
+
+def contact(request, plantilla="contact.html"):
+    return render(request, plantilla)
+
+
+def portfolio(request, plantilla="portfolio.html"):
+    return render(request, plantilla)
